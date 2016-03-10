1d7b2809e16097e600bea27589514b218a43ed42
jose valencia <jose valencia>
2016-03-10 12:43:12 -0800

    there's more random ships now

--- haskell/battleship.hs
+++ haskell/battleship.hs
@@ -2,13 +2,13 @@ import Data.Char (ord)
 
 import System.Random
 
-randomShip2 = ["(2,4);(2,5)"]
+randomShip2 = ["(2,4);(2,5)", "(1,7);(2,7)", "(1,4);(1,5)"]
 
-randomShip3 = ["(5,4);(5,5);(5,6)"]
+randomShip3 = ["(5,4);(5,5);(5,6)", "(8,3);(8,4);(8,5)", "(5,2);(5,3);(5,4)"]
 
-randomShip4 = ["(4,8);(5,8);(6,8);(7,8)"]
+randomShip4 = ["(4,8);(5,8);(6,8);(7,8)", "(1,2);(2,2);(3,2);(4,2)", "(6,3);(6,4);(6,5);(6,6)"]
 
-randomShip5 = ["(3,1);(4,1);(5,1);(6,1);(7,1)"]
+randomShip5 = ["(3,1);(4,1);(5,1);(6,1);(7,1)", "(4,7);(5,7);(6,7);(7,7);(8,7)", "(3,3);(3,4);(3,5);(3,6);(3,7)"]
 
 shootRandom = ["(1,1)", "(1,2)", "(1,3)", "(1,4)", "(1,5)", "(1,6)", "(1,7)", "(1,8)",
                "(2,1)", "(2,2)", "(2,3)", "(2,4)", "(2,5)", "(2,6)", "(2,7)", "(2,8)",
@@ -252,7 +252,7 @@ play names fields ships = do
 playAI :: [String] -> [Field] -> [[Ship]] -> IO ()
 playAI names fields ships = do
                             putStrLn ("\n" ++ head names ++ "'s turn")
-                            printShips (head fields) (head ships)
+                            --printShips (head fields) (head ships)
                             printField (last names) (last fields) (last ships)
                             (newField, newShipList) <- fireWithEveryShipAI (last fields, last ships) (head ships)
                             if length newShipList == 0 then
